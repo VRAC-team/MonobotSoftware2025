7c506bc04a7ec46bc80ac6f3b21d6c874162540f
monowii <monowii@hotmail.fr>
2025-05-03 01:54:48 +0200

    raspiboard: ensure loopback is off at setup

--- raspiboard/setup.sh
+++ raspiboard/setup.sh
@@ -5,8 +5,11 @@ set -ex
 # isolcpus=3
 
 # enable can0
-sudo ip link set can0 up type can bitrate 1000000 restart-ms 100
-sudo ip link set can0 txqueuelen 100
+sudo ip link set can0 down
+sudo ip link set can0 type can bitrate 1000000 loopback off restart-ms 100
+sudo ip link set can0 txqueuelen 64
+sudo ip link set can0 up
+ip -details link show can0
 
 # disable CPU frequency scaling
 echo performance | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
